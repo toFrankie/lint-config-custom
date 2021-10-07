7caf1f3bdde8b295969aea5c8fa7883aa30646e3
Frankie <1426203851@qq.com>
2021-10-07 12:05:28 +0800

    feat: add Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,22 @@
+// More: https://prettier.io/docs/en/options.html
+module.exports = {
+  printWidth: 160,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  quoteProps: 'as-needed',
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf'
+}
